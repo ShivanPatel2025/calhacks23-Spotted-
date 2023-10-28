91a367471aa4675a506cb568bffe4713f14702e1
Shivan Patel <shivanpatel@berkeley.edu>
2023-10-27 23:09:54 -0700

    nav bar

--- navigation/AppNavigation.js
+++ navigation/AppNavigation.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
-import { useNavigation } from '@react-navigation/native'; // Import useNavigation hook
+import { useNavigation } from '@react-navigation/native';
+import { Image } from 'react-native';
 
 import HomeScreen from "../components/HomeScreen.js";
 import DetailsScreen from "../components/DetailsScreen.js";
@@ -10,6 +12,37 @@ import WelcomeScreen from '../components/WelcomeScreen.js';
 import SignupScreen from "../components/SignupScreen.js";
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function MainTabs() {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconSource;
+
+          if (route.name === 'Home') {
+            iconSource = require("../assets/NavHome.png");
+          } else if (route.name === 'Details') {
+            iconSource = require("../assets/NavLeaf.png");
+          } else if (route.name === 'Plants') {
+            iconSource = require("../assets/NavAdd.png");
+          }
+
+          return <Image source={iconSource} style={{ width: size, height: size, tintColor: color }} />;
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: '#B5DCAB',
+        inactiveTintColor: 'gray',
+      }}
+    >
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Details" component={DetailsScreen} />
+      <Tab.Screen name="Plants" component={PlantsScreen} />
+    </Tab.Navigator>
+  );
+}
 
 function AppNavigation() {
   const navigation = useNavigation();
@@ -19,14 +52,12 @@ function AppNavigation() {
       navigation.navigate('Welcome');
     }, 5000);
 
-    // Clear the timer when the component is unmounted
     return () => clearTimeout(timer);
   }, [navigation]);
+
   return (
     <Stack.Navigator initialRouteName="Loading" headerMode="none">
-      <Stack.Screen name="Home" component={HomeScreen} />
-      <Stack.Screen name="Details" component={DetailsScreen} />
-      <Stack.Screen name="Plants" component={PlantsScreen} />
+      <Stack.Screen name="Main" component={MainTabs} />
       <Stack.Screen name="Loading" component={LoadingScreen} />
       <Stack.Screen name="Welcome" component={WelcomeScreen} />
       <Stack.Screen name="Signup" component={SignupScreen} />
